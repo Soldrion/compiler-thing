8a248b8c8c3d57b996f9c18f3a22c8f3d18357db
Soldrion <78869992+Soldrion@users.noreply.github.com>
2025-12-07 17:39:24 -0500

    main.cpp setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+int main(int argc, char* argv[]{
+    if (argc != 2) {
+        std::cerr << "Incorrect usage. Correct usage is..." << std::endl;
+        std::cerr << "hydro <input.hy>" << std::endl;
+        return EXIT_FAILURE;
+}
+    std::cout << argv[1] << std::endl;
+    return EXIT_SUCCESS;
 }
