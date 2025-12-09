11c08bcc06565974b2e0cf7a9f56113e435d1195
Soldrion <78869992+Soldrion@users.noreply.github.com>
2025-12-09 14:11:04 -0500

    From the hydro repo

--- src/tokenization.hpp
+++ src/tokenization.hpp
@@ -1,82 +1,85 @@
 #pragma once
+
 #include <string>
 #include <vector>
-#include <optional>
-enum class TokenType {
-    _exit,
-    int_lit,
-    semi
-};
+
+enum class TokenType { exit, int_lit, semi };
 
 struct Token {
     TokenType type;
-    int line;
     std::optional<std::string> value {};
 };
 
-
 class Tokenizer {
 public:
-    inline explicit Tokenizer(const std::string src)
+    inline explicit Tokenizer(std::string src)
         : m_src(std::move(src))
     {
     }
 
-    inline std::vector<Token> tokenize() {
+    inline std::vector<Token> tokenize()
+    {
         std::vector<Token> tokens;
         std::string buf;
-
-        while (peek().has_value()) {
-            if (std::isalpha(peek().value())) {
+        while (peak().has_value()) {
+            if (std::isalpha(peak().value())) {
                 buf.push_back(consume());
-                while (peek().has_value() && std::isalnum(peek().value())) {
+                while (peak().has_value() && std::isalnum(peak().value())) {
                     buf.push_back(consume());
                 }
                 if (buf == "exit") {
-                    tokens.push_back({.type =  TokenType::_exit});
+                    tokens.push_back({ .type = TokenType::exit });
                     buf.clear();
                     continue;
-                } else {
-                    std::cerr << "Failed to read token 'exit'" << std::endl;
+                }
+                else {
+                    std::cerr << "You messed up!" << std::endl;
                     exit(EXIT_FAILURE);
                 }
-            } else if (std::isdigit(peek().value())) {
+            }
+            else if (std::isdigit(peak().value())) {
                 buf.push_back(consume());
-                while (peek().has_value() && std::isdigit(peek().value())) {
+                while (peak().has_value() && std::isdigit(peak().value())) {
                     buf.push_back(consume());
                 }
-                tokens.push_back({.type= TokenType::int_lit, .value = buf});
+                tokens.push_back({ .type = TokenType::int_lit, .value = buf });
                 buf.clear();
                 continue;
-            }else if (peek().value() == ';') {
-                buf.push_back(consume());
+            }
+            else if (peak().value() == ';') {
+                consume();
+                tokens.push_back({ .type = TokenType::semi });
                 continue;
-        } else if (std::isspace(peek().value())) {
-            buf.push_back(consume());
-            continue;
-        } else {
-            std::cerr << "something messed up bro idk" << std::endl;
-            exit(EXIT_FAILURE);
-        }
+            }
+            else if (std::isspace(peak().value())) {
+                consume();
+                continue;
+            }
+            else {
+                std::cerr << "something messed up bro idk" << std::endl;
+                exit(EXIT_FAILURE);
+            }
         }
         m_index = 0;
         return tokens;
     }
 
 private:
-    [[nodiscard]] std::optional<char> peek(int ahead = 1) const {
-        if (m_index +ahead >= m_src.length()) {
+    [[nodiscard]] inline std::optional<char> peak(int ahead = 1) const
+    {
+        if (m_index + ahead > m_src.length()) {
             return {};
-        } else {
+        }
+        else {
             return m_src.at(m_index);
         }
     }
 
-    char consume() {
+    inline char consume()
+    {
         return m_src.at(m_index++);
     }
 
     const std::string m_src;
-
-    int m_index = 0;
+    size_t m_index = 0;
 };
